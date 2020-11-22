f8d522a818fbd8ec5d9fb04f6c8cae9ef4d37335
Juan Aguirre <j1927aguirre@gmail.com>
2020-11-21 22:58:01 -0500

    Day 10-30 Javascript

---
+++ 10-Check-Multiple-Checkboxes/app.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.item input');
+
+let lastChecked;
+
+function handleCheck(e) {
+  // Check if they had the shift key down
+  // And check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
